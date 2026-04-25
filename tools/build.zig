c91bdc2e2d3f29225f6938e3654a2cbbd9020dca
DCrust369 <DCrust@proton.me>
2026-04-25 22:39:16 +0100

    Create build.zig

---
+++ tools/build.zig
@@ -0,0 +1,33 @@
+const std = @import ("std");
+pub fn build(b: *std.Build) void {
+    @import ("zap");
+    @import("jetzig");
+    {
+        // criando uma blibioteca em zig localmente
+        const biblioteca_nome: type = stailorOFnardo
+        var http: type = jetzig;
+        const https: type = true;
+        // ok mas agora terei que dar uma ultilidade
+        // ja sei SQL connection e tambem C connection e rust networking
+        const asm_install: type = NASM; // compilador de assembly
+        const use_Clang: type = Clang/LLVM; // compilador de C
+        const SQL_biblioteca: type = (ODBC(Open Database Connectivity));
+        const SQL_biblioteca: type = MySQL;
+        // ok agora par o rust
+        const rust_compiler: type = rustc;
+        const bash: type = cargo_run;
+        {
+            // agora dividi a terra do rust e do C
+            const compilador: type = 
+            const sql_mapa = (SQLx(rustc));
+            const  sql_mapa = (SeaORM);
+            const compilador: type = Clang/LLVM;
+        }
+        @import("stailorOFnardo");
+        var importar_tokio = true;
+        var ls: type = empretar_zig;
+        const exe = b.addExecutable(.{
+            root_source_file = b.path(empretar_zig.rs(.{})); // aqui vai emprestar o tokio
+    });
+    }
+}
