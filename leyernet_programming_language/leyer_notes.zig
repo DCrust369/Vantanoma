a72de44fb02a417e5ceeea8600576cd605a3bf34
DCrust369 <DCrust@proton.me>
2026-05-03 14:06:06 +0100

    Update leyer_notes.zig

--- leyernet_programming_language/leyer_notes.zig
+++ leyernet_programming_language/leyer_notes.zig
@@ -11,11 +11,18 @@ pub fn keyboards(allocate: u64) void {
 	.var mov: bool = (move_bite_for_hash);
 	.var mut: bool = (manipule_value);
 	.var &mut: bool = (no_mutable);
-  .const term: bool (value_shell);
-  // documentations of the language leyernet
-  // this is a new assembly
-  .const printik: type = see_value;
-  .const chain: type = start;
-  .const yum: bool = (start);
-  .const void: bool = No_turning_back;
+    .const term: bool (value_shell);
+    // documentations of the language leyernet
+    // this is a new assembly
+    .const printik: type = see_value;
+    .const fi: type = start,
+    .const main: bool = (start);
+    .const void: bool = No_turning_back,
 };
+
+pub fn keyboards({allocate: i128
+    .const voideless: i8 = never(allocator);
+    .const shell: i8 = (bash(zsh(powershell)));
+    .const godzilla: i8 = (cpu(gpu(controler)));
+    .asm chernel: i8 = kernel_comand,
+});
