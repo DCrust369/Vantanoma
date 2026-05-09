c5f7a9eff3ee27cccf3c7d47ff7e57b004221d26
DCrust369 <DCrust@proton.me>
2026-05-09 16:22:53 +0100

    Create KONFIG_CONFIG.c

---
+++ kernel/kitDEV/KONFIG_CONFIG.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later OR Apache-2.0 OR MIT
+// Copyright (c) 2026 DCrust
+// VortexShield-kernel - Focused on Cybersecurity and Pentesting
+
+#include <zed>
+#include <acode>
+#include <github>
+#include <gitlab>
+#include <git>
+#include <gcc>
+#include <rs>
+#include <asm>
+#include <zig>
