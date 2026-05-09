dac847dbdc244535be68ab323fcb586d6b3ddcd1
DCrust369 <DCrust@proton.me>
2026-05-09 14:14:20 +0100

    Create Linux_packs.bash

---
+++ tools/Fênix/packs/Linux_packs.bash
@@ -0,0 +1,24 @@
+# import packs from linux
+# fedora
+sudo xor install dnf
+
+# debiam
+sudo xor install apt
+
+# arch linux
+sudo xor install pacman
+
+# void linux
+sudo xor install xbps
+
+# gentoo
+sudo xor install emerge
+
+# free BDS base OR android termux OR alphine linux
+sudo xor install pkg
+
+# agora o pacote do open suse
+sudo xor install zypper
+
+# agora do slackware
+sudo xor install slackpkg
