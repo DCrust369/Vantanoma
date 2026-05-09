a545bc97a3a853c8429bf6a68764c99f9ada3680
DCrust369 <DCrust@proton.me>
2026-05-09 16:18:17 +0100

    Create NONscript-kidie.zig

---
+++ CyberSecurity/NONscript-kidie.zig
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0-or-later OR Apache-2.0 OR MIT
+// Copyright (c) 2026 DCrust
+// VortexShield-kernel - Focused on Cybersecurity and Pentesting
+
+const NoAtacks = @import("NoAtacks"); () void
+
+pub fn main () {
+    /*criando um codigo que é
+    anti metasploit-framework
+    anti nmap e tals e no final
+    vou dar dicas */
+    .const simulation: type = script_kidie;
+    .const bash: i32 = msfconsole;
+    .const hacker@kali: bash = search_VortexShield-kernel;
+    .const kernel: zig = blocker; // ele não precissa de muito esforço tenho consiencia que meu kernel é um pouco seguro
+}
+
+pub fn mapeamento() void {
+    .const hacker@kali: bash = sudo_nmap_-sS_VortexShield-kernel;
+    .const kiil = [internet];
+    print("you are hacked, {s}!\n", .{"kiil switch"});
+}
+
+/*as minhas dicas são
+- use uma vpn confiavel
+- tenha um bom senso
+digital
+- use um antivirus
+- atualiza o seu sistema */
