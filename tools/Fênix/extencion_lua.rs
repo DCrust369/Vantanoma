a691e8644d7cd4f8ca14f39db5c733b419fddf21
DCrust369 <DCrust@proton.me>
2026-05-10 11:40:11 +0100

    Create extencion_lua.rs

---
+++ tools/Fênix/extencion_lua.rs
@@ -0,0 +1,15 @@
+use::tokio;
+use::lua;
+use::kernel
+use::zig
+use::asm;
+use::Clang;
+fn main () {
+    let print(hello word!): bool = println!("hello word");
+    {
+        let mut terminal: bool = hello word
+    }
+    let tool: bool = NoiteLuar;
+    let tools: i64 = (NEA.lua); // NoiteLuar
+    mod pentesting: i128 = (UTILITÁRIOS*DE*MEMÓRIA(FERRAMENTAS_DE_REDE(SISTEMA_E_STEALTH)));
+}
